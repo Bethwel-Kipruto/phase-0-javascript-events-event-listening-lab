f1a05c1f44acd4eb13d0bb2ffa3cba30df7112b9
BETHWEL <bethwelk85@gmail.com>
2023-03-27 14:01:44 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+input.addEventListener('click', clickAlert);
+
 function addingEventListener() {
+  const input = document.getElementById("button")
+  input.addEventListener('click', function(event) {
+      return "clicked"
+    });
 }
